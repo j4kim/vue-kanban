36018425b48d0c3ed3da5041e79f0a11c73a5f8f
Joaquim Perez <jivkim@gmail.com>
2018-11-28 12:09:17 +0100

    store DOM order in card elements and default sort function use this order. Not sure if optimal solution...  🤔

--- src/components/Kanban.vue
+++ src/components/Kanban.vue
@@ -38,7 +38,7 @@
         type: Function,
         default: function(cardA, cardB){
           // return negative value: A before B
-          return cardA.id - cardB.id
+          return cardA.order - cardB.order
         }
       }
     },
@@ -49,11 +49,12 @@
         .on('drag', (el) => {
           el.classList.add('is-moving');
         })
-        .on('drop', (el, target, source, sibling) => {
+        .on('drop', (el, targetUl, sourceUl, sibling) => {
           var cardId = el.dataset.cardId
-          var oldBucketName = source.dataset.bucketName
-          var oldBucket = self.buckets[source.dataset.bucketName]
-          var newBucketName = target.dataset.bucketName
+          var oldBucketName = sourceUl.dataset.bucketName
+          var oldBucket = self.buckets[oldBucketName]
+          var newBucketName = targetUl.dataset.bucketName
+          var newBucket = self.buckets[newBucketName]
 
           // get card object
           var card = oldBucket.find(card => card.id == cardId)
@@ -61,7 +62,18 @@
           // remove from bucket
           self.buckets[oldBucketName] = oldBucket.filter(card => card.id != cardId)
 
-          // move it to new bucket and sort it
+          // iterate over card elements in bucket to assign corresponding card object an order
+          var i = 0
+          Array.from(targetUl.children).forEach(cardLi => {
+            let cardInBucket = newBucket.find(card => card.id == cardLi.dataset.cardId)
+            if(cardInBucket){
+              cardInBucket.order = i++
+            } else {
+              card.order = i++
+            }
+          })
+
+          // move card into new bucket and sort it
           self.buckets[newBucketName].push(card)
           self.buckets[newBucketName].sort(this.sorter)
 
